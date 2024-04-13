0db4ee56cf9fc26244ec2a267232b975cba6c88a
Alexey Borisenko <bgr666@gmail.com>
2024-04-13 19:29:13 +0300

    alex2

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello super {} World!';
+    return 'Hello super {----} World!';
     // comment 1
     // comment 2
     // comment 3
@@ -11,7 +11,7 @@ export class AppService {
     // fasfa
   }
   getNeHello(): string {
-    return 'testest'
+    return 'testest';
     // comment 1
     // comment 2
     // comment 3
