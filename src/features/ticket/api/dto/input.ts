617680e3e6099cd9d9b69eceb0f650136abfbd61
Vladislav Evg <linesgreen@mail.ru>
2024-04-15 02:00:10 +0300

    [FEAT]: create ticket input model with validation

--- src/features/ticket/api/dto/input.ts
+++ src/features/ticket/api/dto/input.ts
@@ -1,1 +1,21 @@
-export class TicketCreateModel {}
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
+
+export class AnswerCreateModel {
+  @IsString()
+  @IsNotEmpty()
+  answer: string;
+  @IsBoolean()
+  isCorrect: boolean;
+}
+
+export class TicketCreateModel {
+  @IsNotEmpty()
+  img: string | null;
+  @IsString()
+  @IsNotEmpty()
+  question: string;
+  @IsString()
+  @IsNotEmpty()
+  help: string;
+  answers: AnswerCreateModel[];
+}
