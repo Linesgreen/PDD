943e9f730095626f90c224f8f5dc43df65799fce
Vladislav Evg <linesgreen@mail.ru>
2024-04-16 15:33:00 +0300

    [FIX]: fix corse AGAIN

--- src/settings/aplly-app-setting.ts
+++ src/settings/aplly-app-setting.ts
@@ -4,9 +4,5 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 
 export const appSettings = (app: INestApplication) => {
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: '*', // Разрешить запросы с любого домена
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Разрешить все стандартные методы
-    allowedHeaders: 'Content-Type, Accept', // Разрешить заголовки
-  });
+  app.enableCors();
 };
